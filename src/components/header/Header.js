3a45ab6fa7dec11d951d807bdb5f2bd24c722920
Tiago Valdrich <tiago.valdrich@hotmail.com>
2021-05-06 21:43:01 -0300

    Fixed mobile menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,66 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar } from "react-bulma-components";
 
 export default class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav
-          className="navbar"
-          role="navigation"
-          aria-label="main navigation"
-          style={{ backgroundColor: "#F5F0EB" }}
-        >
-          <div className="navbar-brand">
-            <Link className="navbar-item" to="/">
-              <img
-                src={process.env.PUBLIC_URL + "/assets/adielson.png"}
-                width="32"
-                height="64"
-                alt="Adielson's restaurant and pastry"
-              />
-              <h6 className="title is-6"> Adielson</h6>
-            </Link>
+  constructor(props) {
+    super(props);
 
-            <a
-              role="button"
-              className="navbar-burger"
-              aria-label="menu"
-              aria-expanded="true"
-              data-target="navbar-items"
-              href="#!"
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
-          </div>
-
-          <div id="navbar-items" className="navbar-menu">
-            <div className="navbar-start">
-              <Link className="navbar-item" to="/">
-                Início
-              </Link>
-              <Link className="navbar-item" to="/menu">
-                Cardápio
-              </Link>
-            </div>
-
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="buttons">
-                  <Link className="button is-danger" to="/register">
-                    <strong>Cadastre-se</strong>
-                  </Link>
-                  <Link className="button is-danger is-light" to="/login">
-                    Realizar login
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
-    );
+    this.state = { isActive: false };
   }
+
+  setIsActive = (value) => {
+    this.setState({ isActive: value });
+  };
+
+  render = () => {
+    return (
+      <Navbar style={{ backgroundColor: "#F5F0EB" }}>
+        <Navbar.Brand>
+          <Navbar.Item renderAs={Link} to="/">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/adielson.png"}
+              width="32"
+              height="64"
+              alt="Adielson's restaurant and pastry"
+            />
+            <h6 className="title is-6"> Adielson</h6>
+          </Navbar.Item>
+          <Navbar.Burger
+            className={this.state.isActive ? "is-active" : ""}
+            onClick={() => this.setIsActive(!this.state.isActive)}
+          />
+        </Navbar.Brand>
+        <Navbar.Menu className={this.state.isActive ? "is-active" : ""}>
+          <Navbar.Container>
+            <Navbar.Item renderAs={Link} to="/">
+              Home
+            </Navbar.Item>
+            <Navbar.Item renderAs={Link} to="/menu">
+              Cardápio
+            </Navbar.Item>
+          </Navbar.Container>
+          <Navbar.Container align="end">
+            <Navbar.Item href="#" renderAs="div">
+              <div className="buttons">
+                <Link className="button is-danger" to="/register">
+                  <strong>Cadastre-se</strong>
+                </Link>
+                <Link className="button is-danger is-light" to="/login">
+                  Realizar login
+                </Link>
+              </div>
+            </Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    );
+  };
 }
