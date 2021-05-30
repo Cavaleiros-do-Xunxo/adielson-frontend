abf55aa7181f91f59244107cb686b0aaed66af9a
Tiago Valdrich <tiago.valdrich@hotmail.com>
2021-05-30 20:44:00 -0300

    Finished crud structure for menu register

--- src/components/menu-register-item/MenuRegisterItem.js
+++ src/components/menu-register-item/MenuRegisterItem.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { Content, Heading, Media, Image } from "react-bulma-components";
+import { Button, Content, Heading, Media, Image } from "react-bulma-components";
 
 const MenuRegisterItem = (props) => {
   return (
     <Media>
       <Media.Item align="left">
-        <Image src={props.imageUrl} size={64} />
+        <Image
+          src={
+            props.imageUrl != null && props.imageUrl !== ""
+              ? props.imageUrl
+              : "http://bulma.io/images/placeholders/128x128.png"
+          }
+          size={64}
+        />
       </Media.Item>
       <Media.Item align="center">
         <Content>
           <Heading size={4}>{props.title}</Heading>
           <Heading subtitle size={6} weight="normal">
-            {props.description}
+            <p>{props.description}</p>
+            <p style={{ color: "#48c774" }}>R${props.price}</p>
           </Heading>
         </Content>
+      </Media.Item>
+      <Media.Item align="right">
+        <Button
+          color="danger"
+          rounded={true}
+          onClick={() => {
+            props.onDelete(props.renderId);
+          }}
+          style={{ marginTop: "10px" }}
+        >
+          Remover do cardápio
+        </Button>
       </Media.Item>
     </Media>
   );
--- src/pages/menu-register/MenuRegister.js
+++ src/pages/menu-register/MenuRegister.js
@@ -1,9 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
+import uuid from "react-uuid";
 import { motion } from "framer-motion";
-import { Box, Block, Container, Heading, Button } from "react-bulma-components";
+import {
+  Box,
+  Block,
+  Button,
+  Container,
+  Form,
+  Heading,
+} from "react-bulma-components";
 import MenuRegisterItem from "../../components/menu-register-item/MenuRegisterItem";
 
+const config = {
+  defaultNewProduct: {
+    renderId: "",
+    title: "",
+    description: "",
+    price: "",
+    imageUrl: "",
+  },
+};
+
 const MenuRegister = (props) => {
+  const [isEditMode, setEditMode] = useState(false);
+  const [canShowAddMenuItem, setCanShowAddMenuItem] = useState(false);
+  const [newProduct, setNewProduct] = useState(config.defaultNewProduct);
+  const [menuItems, setMenuItems] = useState([]);
+
+  const handleNewProductInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
+
+  const addNewMenuItem = () => {
+    const _menuItems = menuItems;
+
+    newProduct.renderId = uuid();
+    _menuItems.push(newProduct);
+
+    setMenuItems(_menuItems);
+    setNewProduct(config.defaultNewProduct);
+    setCanShowAddMenuItem(false);
+  };
+
+  const showAddMenuItem = () => {
+    if (!canShowAddMenuItem) {
+      return;
+    }
+
+    return (
+      <Box>
+        <Heading size={4}>Adicionar novo item</Heading>
+        <Form.Field>
+          <Form.Label>Título</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="text"
+              name="title"
+              placeholder="Título do item do cardápio"
+              onChange={(e) => handleNewProductInputChange(e)}
+            />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>Descrição</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="text"
+              name="description"
+              placeholder="Descrição do item do cardápio"
+              onChange={(e) => handleNewProductInputChange(e)}
+            />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>Preço</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="text"
+              name="price"
+              placeholder="15.00"
+              onChange={(e) => handleNewProductInputChange(e)}
+            />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>Url da imagem</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="text"
+              name="imageUrl"
+              placeholder="Url da imagem do item"
+              onChange={(e) => handleNewProductInputChange(e)}
+            />
+          </Form.Control>
+        </Form.Field>
+        <hr />
+        <Button.Group justifyContent="center">
+          <Button
+            color="danger"
+            onClick={() => {
+              setNewProduct(config.defaultNewProduct);
+              setCanShowAddMenuItem(false);
+            }}
+          >
+            Cancelar
+          </Button>
+          <Button
+            color="success"
+            onClick={() => {
+              addNewMenuItem();
+            }}
+          >
+            Adicionar ao cardápio
+          </Button>
+        </Button.Group>
+      </Box>
+    );
+  };
+
+  const showAddMenuItemButton = () => {
+    if (!isEditMode || canShowAddMenuItem) {
+      return;
+    }
+
+    return (
+      <Button
+        color="success"
+        onClick={() => {
+          setCanShowAddMenuItem(true);
+        }}
+        rounded={true}
+      >
+        <i className="fas fa-plus" style={{ marginRight: "5px" }}></i>Novo item
+      </Button>
+    );
+  };
+
+  const deleteMenuItem = (id) => {
+    setMenuItems(
+      menuItems.filter((item) => {
+        return item.renderId !== id;
+      })
+    );
+    console.log(menuItems, id);
+  };
+
+  const showMenuItems = () => {
+    const products = [];
+
+    for (const product of menuItems) {
+      products.push(
+        <MenuRegisterItem
+          key={product.renderId}
+          renderId={product.renderId}
+          imageUrl={product.imageUrl}
+          title={product.title}
+          description={product.description}
+          price={product.price}
+          onDelete={deleteMenuItem}
+        />
+      );
+    }
+
+    return products;
+  };
+
   return (
     <Container
       renderAs={motion.div}
@@ -14,31 +176,22 @@ const MenuRegister = (props) => {
       <Box style={{ marginTop: "10px" }}>
         <Block style={{ display: "flex", justifyContent: "space-between" }}>
           <Heading>Cardápio do dia</Heading>
-          <Button
-            color="danger"
-            rounded={true}
-            onClick={() => {
-              props.history.push("/menuregister");
-            }}
-          >
-            Editar
-          </Button>
+          <Button.Group>
+            {showAddMenuItemButton()}
+            <Button
+              color="danger"
+              rounded={true}
+              onClick={() => {
+                setEditMode(!isEditMode);
+              }}
+            >
+              {isEditMode ? "Finalizar edição" : "Editar"}
+            </Button>
+          </Button.Group>
         </Block>
-        <MenuRegisterItem
-          imageUrl={"http://bulma.io/images/placeholders/128x128.png"}
-          title={"Marmita"}
-          description={"Arroz, feijão e frango grelhado."}
-        />
-        <MenuRegisterItem
-          imageUrl={"http://bulma.io/images/placeholders/128x128.png"}
-          title={"Marmita"}
-          description={"Arroz, feijão e frango grelhado."}
-        />
-        <MenuRegisterItem
-          imageUrl={"http://bulma.io/images/placeholders/128x128.png"}
-          title={"Marmita"}
-          description={"Arroz, feijão e frango grelhado."}
-        />
+        <hr />
+        {showAddMenuItem()}
+        {showMenuItems()}
       </Box>
     </Container>
   );
